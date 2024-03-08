ef34144773bd261b5b9efeb8b6b9a161a78c6f4e
Iris Veress <irisveress@gmail.com>
2024-03-08 19:10:01 +0200

    added tracking button

--- app/src/main/java/com/example/itfest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/itfest/ui/home/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.itfest.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.itfest.databinding.FragmentHomeBinding
@@ -28,10 +30,11 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val trackBttn = binding.trackBttn
+        trackBttn.setOnClickListener {
+            
         }
+
         return root
     }
 
