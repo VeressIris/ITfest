47c5ba44d9fa204105c3e5f638afea8e330b8e80
Iris Veress <irisveress@gmail.com>
2024-03-09 02:14:51 +0200

    organized some things

--- app/src/main/java/com/example/itfest/ui/dashboard/PetFragment.kt
+++ app/src/main/java/com/example/itfest/ui/dashboard/PetFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.itfest.databinding.FragmentAnimalBinding
 import com.example.itfest.databinding.FragmentStatsBinding
 
-class DashboardFragment : Fragment() {
+class PetFragment : Fragment() {
 
-    private var _binding: FragmentStatsBinding? = null
+    private var _binding: FragmentAnimalBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentStatsBinding.inflate(inflater, container, false)
+        _binding = FragmentAnimalBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
