244971152546a8300c0d976b2d18cf4bd60ac1c7
alexiamates <162727206+alexiamates@users.noreply.github.com>
2024-03-10 00:44:01 +0200

    yfytc
    hgv

--- app/src/main/java/com/example/itfest/classes/Mood.kt
+++
@@ -1,3 +0,0 @@
-package com.example.itfest.classes
-
-data class Mood(val currentMood: String, val message: String?)
--- app/src/main/java/com/example/itfest/classes/Task.kt
+++
@@ -1,8 +0,0 @@
-package com.example.itfest.classes
-
-//considered as todo
-open class Task(val name: String, var completed: Boolean, var frequency: String) {
-    fun markAsCompleted() {
-        completed = true
-    }
-}
